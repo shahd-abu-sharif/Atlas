bbecb5878148b77e0aea55651fd54e5a6b095653
shahd <eng.shahd.abusharif@gmail.com>
2026-06-10 21:35:48 +0300

    chore(app): update main.ts and register new modules in AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Tadreeby API')
+    .setDescription('Auth system API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  const port = process.env.PORT || 6060;
+  await app.listen(port);
+
+  console.log(`Swagger API on http://localhost:${port}/api`);
+
+  // console.log(`Server running on http://localhost:${port}/api`);
+  // console.log(`Swagger at http://localhost:${port}/docs`);
 }
 bootstrap();
